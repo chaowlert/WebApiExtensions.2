7d0164d6071f7a45f947627706ee6e683d05abd9
chaowlert <chaowlert@gmail.com>
2017-07-10 21:24:44 +0700

    CreateController, ProfileFromName

---
+++ WebApiExtensions.2/Services/ApiControllerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using DryIoc.WebApi;
+using DryIoc;
+
+namespace WebApiExtensions.Services
+{
+    public static class ApiControllerExtensions
+    {
+        public static TCtrl CreateController<TCtrl>(this ApiController mainCtrl) where TCtrl : ApiController, new()
+        {
+            var container = ((DryIocDependencyScope)mainCtrl.Request.GetDependencyScope()).ScopedContainer;
+            var instance = (TCtrl)container.Resolve(typeof(TCtrl));
+            if (instance.Request != null)
+                return instance;
+
+            instance.RequestContext = mainCtrl.RequestContext;
+            instance.Request = mainCtrl.Request;
+            instance.ActionContext = mainCtrl.ActionContext;
+            instance.Configuration = mainCtrl.Configuration;
+            instance.ControllerContext = mainCtrl.ControllerContext;
+            instance.Url = mainCtrl.Url;
+            instance.User = mainCtrl.User;
+            return instance;
+        }
+    }
+}
--- WebApiExtensions.2/Services/ImageHelper.cs
+++ WebApiExtensions.2/Services/ImageHelper.cs
@@ -1,5 +1,10 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.IO;
+using System.Linq;
 using ImageProcessor;
 using ImageProcessor.Imaging;
 using ImageProcessor.Imaging.Formats;
@@ -23,5 +28,43 @@ namespace WebApiExtensions.Services
                 dest.Position = 0;
             }
         }
+
+        private static readonly Color[] colorSet = {
+            Color.FromArgb(145, 60, 205),
+            Color.FromArgb(241, 95, 116),
+            Color.FromArgb(247, 109, 60),
+            Color.FromArgb(247, 216, 66),
+            Color.FromArgb(44, 168, 194),
+            Color.FromArgb(152, 203, 74),
+            Color.FromArgb(131, 144, 152),
+            Color.FromArgb(84, 129, 230),
+        };
+        public static void ProfileFromName(Stream dest, int size, string name)
+        {
+            using (var bitmap = new Bitmap(size, size))
+            using (var img = new ImageFactory())
+            {
+                var background = colorSet[Math.Abs(name.GetHashCode()) % 8];
+                var text = getValidChar(name).ToString();
+                img.Load(bitmap)
+                    .BackgroundColor(background)
+                    .Watermark(new TextLayer
+                    {
+                        FontColor = Color.White,
+                        FontSize = size * 7 / 10,
+                        Style = FontStyle.Bold,
+                        Text = text
+                    })
+                    .Format(jpeg)
+                    .Save(dest);
+                dest.Position = 0;
+            }
+        }
+        private static readonly HashSet<char> validChars = "กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฤฦ".ToHashSet();
+        private static char getValidChar(string name)
+        {
+            return name.Take(2).Where(validChars.Contains).Select(c => (char?)c).FirstOrDefault() ?? name.ToUpper()[0];
+        }
+
     }
 }
